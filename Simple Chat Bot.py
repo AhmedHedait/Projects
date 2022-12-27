70b6be6b254ef3fa5c97813b50c09a4ddd90e9af
AhmedHedait <121409810+AhmedHedait@users.noreply.github.com>
2022-12-28 01:05:04 +0200

    Update Simple Chat Bot.py

--- Simple Chat Bot.py
+++ Simple Chat Bot.py
@@ -1,3 +1,5 @@
+# A simple console chat bot which is interacts with the user by answering some questions.
+
 def greet(bot_name, birth_year):
     print('Hello! My name is ' + bot_name + '.')
     print('I was created in ' + birth_year + '.')
